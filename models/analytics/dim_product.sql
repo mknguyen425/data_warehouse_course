9623d7aad07efaf6d40d240e1b4c5b4101bc8bcb
mknguyen425 <mknguyen425@gmail.com>
2023-03-11 13:28:48 -0800

    cte rename

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,5 +1,20 @@
+WITH dim_product__source AS (
+  SELECT *
+  FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+)
+
+, dim_product__rename_column AS(
+  SELECT 
+    stock_item_id AS product_key
+  , stock_item_name  AS product_name
+  , brand AS brand_name
+  FROM dim_product__source
+)
+
 SELECT 
-  CAST(stock_item_id AS INTEGER) AS product_key
-  , CAST(stock_item_name AS STRING) AS product_name
-  , CAST(brand AS STRING) AS brand_name
-FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
+  CAST( product_key AS INTEGER) AS product_key
+  , CAST (product_name AS STRING) AS product_name
+  , CAST (brand_name AS STRING) AS brand_name
+FROM dim_product__rename_column
+
+
