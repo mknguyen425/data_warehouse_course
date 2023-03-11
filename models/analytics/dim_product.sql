9c075c8ee14790ca7cbb50bb671bc816f49253ed
mknguyen425 <mknguyen425@gmail.com>
2023-03-11 13:35:11 -0800

    cte

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -10,11 +10,18 @@ WITH dim_product__source AS (
   , brand AS brand_name
   FROM dim_product__source
 )
-
+, dim_product__cast_type AS(
 SELECT 
   CAST( product_key AS INTEGER) AS product_key
   , CAST (product_name AS STRING) AS product_name
   , CAST (brand_name AS STRING) AS brand_name
 FROM dim_product__rename_column
+)
+
+SELECT 
+   product_key
+  ,product_name
+  ,brand_name
+FROM dim_product__cast_type
 
 
