be4fb5c2a9bd03aab6e03626e54feccf39c5a28d
mknguyen425 <mknguyen425@gmail.com>
2023-03-11 12:59:31 -0800

    cast

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -1,3 +1,5 @@
 SELECT 
-  *
+  CAST(stock_item_id AS INTEGER) AS product_key
+  , CAST(stock_item_name AS STRING) AS product_name
+  , CAST(brand AS STRING) AS brand_name
 FROM `vit-lam-data.wide_world_importers.warehouse__stock_items`
--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -1,5 +1,8 @@
 SELECT 
-   order_line_id AS sales_order_line_key,
-   quantity,
-   unit_price
+   CAST(order_line_id AS INTEGER) AS sales_order_line_key, 
+   CAST(quantity AS INTEGER) AS quantity,
+   CAST(unit_price AS NUMERIC) AS unit_price, 
+   CAST(stock_item_id AS INTEGER) AS product_key,
+   CAST(quantity AS INTEGER)* CAST(unit_price AS NUMERIC) AS gross_amount
+   
 FROM `vit-lam-data.wide_world_importers.sales__order_lines`
