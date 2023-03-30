3e3828622cf83b980770dee4a8d4b25395cd3a71
mknguyen425 <mknguyen425@gmail.com>
2023-03-29 21:18:39 -0700

    m

--- models/analytics/dim_product.sql
+++ models/analytics/dim_product.sql
@@ -9,6 +9,7 @@ WITH dim_product__source AS (
   , stock_item_name  AS product_name
   , brand AS brand_name
   , supplier_id AS supplier_key
+  , is_chiller_stock AS is_chiller_stock
   FROM dim_product__source
 )
 , dim_product__cast_type AS(
@@ -17,6 +18,8 @@ SELECT
   , CAST (product_name AS STRING) AS product_name
   , CAST (brand_name AS STRING) AS brand_name
   , CAST (supplier_key AS INTEGER) AS supplier_key
+  , CAST (is_chiller_stock AS BOOLEAN) AS is_chiller_stock
+
 FROM dim_product__rename_column
 )
 
@@ -26,6 +29,7 @@ SELECT
   , dim_product.brand_name
   , dim_product.supplier_key
   , dim_supplier.supplier_name
+  , dim_product.is_chiller_stock
 FROM dim_product__cast_type AS dim_product
 LEFT JOIN {{ ref('dim_supplier') }} AS dim_supplier
   ON dim_product.supplier_key = dim_supplier.supplier_key
